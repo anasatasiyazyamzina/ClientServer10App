9699cd434f3c5f445b7e91c37c70d98bfa886a81
Anastasiya Zyazmina <45993670+anastaiyazyamzina@users.noreply.github.com>
2020-05-04 12:09:34 +0400

    Add files via upload

---
+++ TCPCLient/Tcp.Client/ClientAn.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SomeProject.TcpClient
+{
+    partial class ClientAn
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sendMsgBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.Messages = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendMsgBtn
+            // 
+            this.sendMsgBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.sendMsgBtn.BackColor = System.Drawing.Color.Silver;
+            this.sendMsgBtn.Location = new System.Drawing.Point(31, 359);
+            this.sendMsgBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sendMsgBtn.Name = "sendMsgBtn";
+            this.sendMsgBtn.Size = new System.Drawing.Size(192, 68);
+            this.sendMsgBtn.TabIndex = 0;
+            this.sendMsgBtn.Text = "Send Message";
+            this.sendMsgBtn.UseVisualStyleBackColor = false;
+            this.sendMsgBtn.Click += new System.EventHandler(this.OnMsgBtnClick);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox.Location = new System.Drawing.Point(12, 159);
+            this.textBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox.MinimumSize = new System.Drawing.Size(514, 178);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(514, 178);
+            this.textBox.TabIndex = 1;
+            // 
+            // labelRes
+            // 
+            this.labelRes.Location = new System.Drawing.Point(0, 0);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(100, 23);
+            this.labelRes.TabIndex = 7;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Location = new System.Drawing.Point(286, 359);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 68);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Messages
+            // 
+            this.Messages.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Messages.BackColor = System.Drawing.Color.SeaShell;
+            this.Messages.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Messages.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Messages.Font = new System.Drawing.Font("Rockwell Nova Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Messages.HideSelection = false;
+            this.Messages.Location = new System.Drawing.Point(12, 54);
+            this.Messages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Messages.MaxLength = 32000;
+            this.Messages.Multiline = true;
+            this.Messages.Name = "Messages";
+            this.Messages.ReadOnly = true;
+            this.Messages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Messages.Size = new System.Drawing.Size(514, 64);
+            this.Messages.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(12, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Write message:";
+            // 
+            // lblMess
+            // 
+            this.lblMess.AutoSize = true;
+            this.lblMess.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblMess.Location = new System.Drawing.Point(12, 23);
+            this.lblMess.Name = "lblMess";
+            this.lblMess.Size = new System.Drawing.Size(154, 17);
+            this.lblMess.TabIndex = 6;
+            this.lblMess.Text = "Messages from Server:";
+            // 
+            // ClientAn
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(536, 438);
+            this.Controls.Add(this.lblMess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Messages);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.sendMsgBtn);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ClientAn";
+            this.Text = "Client application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendMsgBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMess;
+        protected System.Windows.Forms.TextBox Messages;
+    }
+}
+
---
+++ TCPCLient/Tcp.Client/ClientAn.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Forms;
+using SomeProject.Library.Client;
+using SomeProject.Library;
+
+namespace SomeProject.TcpClient
+{
+    public partial class ClientAn : Form
+    {
+        public ClientAn()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Кнопка отправки сообщения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnMsgBtnClick(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            if (textBox.Text != "")
+            {
+                OperationResult res = client.SendMessageToServer(textBox.Text);
+                if (res.Result == Result.OK)
+                {
+                    textBox.Text = "";
+                    // MessageBox.Show("Message was sent succefully!","Success");
+                    Messages.Text += res.Message + Environment.NewLine;
+                }
+                else
+                {
+                    // MessageBox.Show("Cannot send the message to the server.","Exeption");
+                    Messages.Text += "!!! " + res.Message + Environment.NewLine;
+                }
+                timer.Interval = 2000;
+                timer.Start();
+            }
+            else MessageBox.Show("Write a message!","Exeption");
+        }
+        /// <summary>
+        /// таймер
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+        /// <summary>
+        ///  отправка файла на сервер
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Client client = new Client();
+                var res = client.SendFileToServer(fileDialog.FileName);
+                if (res.Result == Result.OK)
+                {
+                    Messages.Text += res.Message + Environment.NewLine;
+                    // MessageBox.Show("File was sent succefully!","Success!");
+                }
+                else
+                {
+                    //MessageBox.Show("Cannot send the file to the server.","Exeption");
+                    Messages.Text += "!!! " + res.Message + Environment.NewLine;
+                }
+ 
+                timer.Interval = 2000;
+                timer.Start();
+            }
+        }
+
+    }
+}
